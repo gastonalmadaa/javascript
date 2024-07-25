6511e01b7f42568a85c5e654be7367a4f96f91d4
gastonalmadaa <gastonalmada2_@hotmail.com>
2024-07-24 22:45:18 -0300

    entrega semana 4

---
+++ main.js
@@ -0,0 +1,53 @@
+// USUARIO
+const nameuser = prompt("Hi! What is your name?:");
+
+let edad = parseInt(prompt("¿How old are you " + nameuser + "?"));
+if (edad >= 18);
+else {alert ("Please, only purchase if you have adult supervision")}
+
+
+// PRODUCTOS Y PRECIO
+/* KEYBOARDS */
+let keyboardrazer1 =    {nombre: "Ornata V3",
+    precio: 69.99};
+console.log(keyboardrazer1);
+
+let keyboardrazer2 =    {nombre: "Blackwidow V3",
+    precio: 179.99};
+console.log(keyboardrazer2);
+
+/* MICES */
+let micerazer1 =    {nombre: "Viper Ultimate",
+precio: 159.99};
+console.log(micerazer1);
+
+let micerazer2 =    {nombre: "Basilisk V3 Pro",
+precio: 159.99};
+console.log(micerazer2);
+
+/* AUDIO */
+let audiorazer1 =   {nombre: "Krakenk V3 Pro",
+precio: 79.99}
+console.log(audiorazer1)
+
+let audiorazer2 =   {nombre: "Nommo Speakers V2 Pro",
+precio: 449.99}
+console.log(audiorazer2)
+
+/* MATS */
+let matrazer1 =     {nombre: "Atlas",
+precio: 99.99}
+console.log(matrazer1)
+
+let matrazer2 =     {nombre: "Goliathus Chroma™",
+precio: 39.99}
+console.log(matrazer2)
+
+/* CHAIRS */
+let chairrazer1 =     {nombre: "Iskur",
+precio: 649.99}
+console.log(chairrazer1)
+
+let chairrazer2 =     {nombre: "Fujin Pro",
+precio: 1049.99}
+console.log(chairrazer2)
